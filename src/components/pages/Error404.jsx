b0d00517c09ddeb0aac56859cb7121ae893c2c54
Matias Salazar <matias39974593@gmail.com>
2024-10-02 07:03:33 -0300

    se realizo pag de error404 con una imagen de fondo

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,7 +1,17 @@
+import error from "../../assets/Error.jpg";
+import { Link } from "react-router-dom";
+
 const Error404 = () => {
-  return <div>
-    <h2>maquetado error</h2>
-  </div>;
+  return (
+    <section className="mainSection text-center">
+      <img src={error} alt="error 404" />
+      <div>
+        <Link className="btn btn-success" to="/">
+          Volver al inicio
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error404;
