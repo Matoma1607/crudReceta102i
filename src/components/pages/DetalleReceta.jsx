5718b97ab0a31cb9c6a12d2174192f910700dbbd
Matias Salazar <matias39974593@gmail.com>
2024-10-02 06:45:23 -0300

    se maqueto DetalleReceta

--- src/components/pages/DetalleReceta.jsx
+++ src/components/pages/DetalleReceta.jsx
@@ -1,9 +1,13 @@
+import { useParams } from 'react-router-dom';
 
 const DetalleReceta = () => {
+    const { id } = useParams();
+  const recetas = JSON.parse(localStorage.getItem('recetas')) || [];
     return (
-        <div>
-            <h2>maquetado Detalle</h2>
-        </div>
+        <div className="container">
+        <h1>Detalle de la Receta</h1>
+        <p>{recetas[id] || 'Receta no encontrada'}</p>
+      </div>
     );
 };
 
