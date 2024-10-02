b565ce3948d2883ca1738481f949c071d893f976
Matias Salazar <matias39974593@gmail.com>
2024-10-01 22:03:22 -0300

    se realizo el maquetado del navbar y footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,14 @@
+import { Container } from 'react-bootstrap';
+
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer className="bg-dark text-light text-center py-3">
+    <Container>
+      <p>&copy; {new Date().getFullYear()} Blog de Recetas</p>
+      <a href="/#" className="text-light mx-2">Política de Privacidad</a>
+      <a href="/#" className="text-light mx-2">Términos de Servicio</a>
+    </Container>
+  </footer>
   );
 };
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,8 +1,50 @@
-const Menu = () => {
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import logo from "../../assets/logo.png";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
+const Menu = ({ usuarioLogueado, setUsuarioLogueado }) => {
+  const navegacion = useNavigate();
+
+  const logout = () => {
+    sessionStorage.removeItem("userKey");
+    setUsuarioLogueado("");
+    navegacion("/");
+  };
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            alt="logo Rolling Coffee"
+            className="img-fluid"
+            width={150}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink end className="nav-link" to="/">
+              Inicio
+            </NavLink>
+            {usuarioLogueado !== "" ? (
+              <>
+                <NavLink end className="nav-link" to="/administrador">
+                  Administrador
+                </NavLink>
+                <Button className="nav-link" onClick={logout}>
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <NavLink end className="nav-link" to="/login">
+                Login
+              </NavLink>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
